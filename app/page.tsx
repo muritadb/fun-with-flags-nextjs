e4cf82a151cc73512ca0f9a45875cd0061c06653
murita <douglasb008@gmail.com>
2025-03-24 09:54:26 -0300

    modified page

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li className="mt-3">Estava CERTO PORRA</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
