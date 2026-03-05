27219c17921509b5c7abb104622e36633b212a91
Murita <muritaa@proton.me>
2026-03-05 19:42:12 -0300

    add post function to api

--- app/services/api.js
+++ app/services/api.js
@@ -1,25 +1,29 @@
 const apiClient = (baseUrl) => ({
   async get(endpoint) {
     try {
-      const response = await fetch(`${baseUrl}${endpoint}`)
+      const response = await fetch(`${baseUrl}${endpoint}`);
 
-      if(!response.ok) {
-        return [null, `HTTP error! status: ${response.statusText}`]
+      if (!response.ok) {
+        return [null, `HTTP error! status: ${response.statusText}`];
       }
 
-      const data = await response.json()
-      return [data, null]
-    } catch(error) {
-      console.log("API request failed: ", error)
-      return [null, error.message]
+      const data = await response.json();
+      return [data, null];
+    } catch (error) {
+      console.log("API request failed: ", error);
+      return [null, error.message];
     }
-  }
-})
+  },
+  async post(endpoint) {
+    return endpoint;
+  },
+});
 
-const api = apiClient('https://restcountries.com/v3.1')
+const api = apiClient("https://restcountries.com/v3.1");
 
 const countriesApi = {
-  getAll: () => api.get('/all?fields=cca3,flags,name,capital,region,population')
-}
+  getAll: () =>
+    api.get("/all?fields=cca3,flags,name,capital,region,population"),
+};
 
-export { countriesApi }
+export { countriesApi };
